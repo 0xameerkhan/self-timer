10e6efd6f6331c16427917b693260bddb223f59e
0xameerkhan <154062533+0xameerkhan@users.noreply.github.com>
2024-05-28 14:50:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.scss";
+
+const App = () => {
+  const date1 = new Date(import.meta.env.VITE_LEFT_DATE);
+  const date2 = new Date();
+  const timeDiff = Math.abs(date2 - date1);
+  const diffMonths = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30));
+  const diffYears = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365));
+  const diffDays = Math.floor((timeDiff / (1000 * 60 * 60 * 24)));
+  const diffWeeks = Math.floor((timeDiff / (1000 * 60 * 60 * 24 * 7)));
+  console.log("months:", diffMonths);
+  return (
+    <div className="app">
+      {/* <p>{diffYears} YEARS</p> */}
+      {/* <p>{diffMonths} MONTHS</p> */}
+      {/* <p>{diffWeeks} WEEKS</p> */}
+      <p>{diffDays} DAYS</p>
+    </div>
+  );
+};
+
+export default App;

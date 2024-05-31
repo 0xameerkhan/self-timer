a94791dc1f955932ce96787ebd87afbe856c3884
0xameerkhan <154062533+0xameerkhan@users.noreply.github.com>
2024-05-31 08:19:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ const App = () => {
   const date1 = new Date(import.meta.env.VITE_LEFT_DATE);
   const date2 = new Date();
   const timeDiff = Math.abs(date2 - date1);
-  const diffMonths = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30));
-  const diffYears = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365));
+  // const diffMonths = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30));
+  // const diffYears = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365));
   const diffDays = Math.floor((timeDiff / (1000 * 60 * 60 * 24)));
-  const diffWeeks = Math.floor((timeDiff / (1000 * 60 * 60 * 24 * 7)));
+  // const diffWeeks = Math.floor((timeDiff / (1000 * 60 * 60 * 24 * 7)));
   return (
     <div className="app">
       {/* <p>{diffYears} YEARS</p> */}

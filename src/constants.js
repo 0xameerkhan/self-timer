2e267d6cd3c610df5a0becc316aee6443d35bee1
0xameerkhan <154062533+0xameerkhan@users.noreply.github.com>
2024-06-12 11:14:16 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const imgLinks = [
+  "https://i.pinimg.com/originals/20/17/f2/2017f28c4a349252731718c556c91e16.gif",
+  "https://media1.tenor.com/m/IJz7HbdC_4EAAAAC/respect-hat.gif",
+  "https://www.icegif.com/wp-content/uploads/2023/07/icegif-929.gif",
+  "https://gifdb.com/images/high/school-of-rock-respect-0uj5i2vn81snzs1e.gif",
+  "https://i.pinimg.com/originals/6e/36/fd/6e36fda1a6273c0174352077e6b187a7.gif"
+];
